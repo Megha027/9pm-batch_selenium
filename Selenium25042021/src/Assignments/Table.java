a04f0ccb758542f0ea8d8f0b11b0a844f68458c2
megha <megha@LAPTOP-7LJGM0EB>
2021-05-29 21:56:48 +0530

    pushing code table

--- Selenium25042021/src/Assignments/Table.java
+++ Selenium25042021/src/Assignments/Table.java
@@ -7,6 +7,7 @@ public class Table {
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter any number ");
 		int num=s.nextInt();
+		System.out.println();
 		for(int i=1; i<=10; i++)
 		{
 			System.out.println(num+"*"+i+"="+num*i);
